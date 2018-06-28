00a7b285bf41c94b2bc3a4bbdffe05231b44f12e
yaypaul <>
2018-06-28 16:42:23 -0400

    Updated to version 1.4 Added Object merging prototype function mergeObject

--- src/util__object.js
+++ src/util__object.js
@@ -1,16 +1,16 @@
 /**
  * util__object
  * @fileoverview Sets up prototype Object Utility functions on JavaScripts Object object.
- * @version 1.3
+ * @version 1.4
  * @author <a href="http://yaypaul.com">YayPaul (Paul West)</a>
- * @copyright 2017 Paul West.
+ * @copyright 2017-2018 Paul West.
  * @license MIT
  */
 
 /**
- * To Query String
+ * Parse Object As Query String
  * @desc Parses a JavaScript Object into a querystring.
- * @return A query string.
+ * @return {string} A query string.
  * @since 1.0
  */
 Object.defineProperty( Object.prototype, 'parseObjectAsQueryString', {
@@ -26,6 +26,33 @@ Object.defineProperty( Object.prototype, 'parseObjectAsQueryString', {
             return encodeURIComponent( key ) + '=' + encodeURIComponent( obj[ key ] );
             
         } ).join( '&' );
+        
+    }
+    
+} );
+
+/**
+ * Merge Object
+ * @desc Merges a JavaScript Object into the existing object.
+ * @param {object} toMerge Object to merge in.
+ * @return {object} The merged object.
+ * @since 1.4
+ */
+Object.defineProperty( Object.prototype, 'mergeObject', {
+    
+    value   : function mergeObject( toMerge ){
+        
+        'use strict';
+
+        var obj = this;
+        
+        for( var key in toMerge ){
+
+            obj[ key ] = toMerge[ key ];
+
+        }
+
+        return obj;
         
     }
     
